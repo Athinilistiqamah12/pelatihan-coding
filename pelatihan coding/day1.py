05a44387c666512eddbc06b15af5ffd2163e7c04
Athinilistiqamah12 <athinilisti@gmail.com>
2024-09-07 11:00:58 +0700

    Add files via upload

---
+++ pelatihan coding/day1.py
@@ -0,0 +1,37 @@
+# a = "apel" # "a" adalah variabel "apel" adalah data 
+# print(a) 
+
+# buah = "jeruk" # ini adalah tipe data 'string'
+# harga = 15000 # ini adalah tipe data 'integer'
+# berat = 1.5 # ini adalah tipe data 'float'
+# beli = True # ini adalah tipe data 'boolean'
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+
+# format string
+# nama = 'Siti'
+# umur = 16
+# alamat = 'jl.buntu'
+# tinggi = 1.75
+
+# print(f"nama{saya {nama}, Saya berusia {umur} tahun, dan tinggi saya {tinggi} meter, saya tinggal {alamat}.")
+
+#input
+# nama = input ("siapa nama kamu?")
+# print (f"Halo, {nama}")
+
+# umur = int(input("umur kamu berapa?"))
+# print(f"{umur} tahun")
+
+print("Halo, perkenalkan nama aku Bot!")
+print("Halo, Bot!")
+nama = input ("nama, kamu siapa?")
+print(f"Namaku {nama}")
+print(f"halo, {nama}. salam kenal!")
+print (f"kamu sibuk apa sekarang, {nama}?")
+kegiatan = input ("kesibukanmu?")
+print (f"aku lagi sibuk {kegiatan} sekarang.")
+print(f"oke, semoga {kegiatan} berhasil.")
